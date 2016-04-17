3d05aec449a7586d43d89c9edf67c747cf7b8a0f
ertage <shv.an.vol@gmail.com>
2016-04-17 21:44:06 +0300

    first commit

---
+++ app/app.js
@@ -0,0 +1,13 @@
+angular.module('sunlook', [])
+
+  .controller('firstController', function() {
+    this.model = {
+      style: "straight",
+      color: "red",
+      skirt: "1",
+      sleeve: "1",
+      decollete: "1",
+      textile: "silk",
+      size: "s"
+    };
+  })
