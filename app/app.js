2a63d0166608e5e405d84568273fb32098bb2953
ertage <shv.an.vol@gmail.com>
2016-04-18 23:07:04 +0300

    added controller for style

--- app/app.js
+++ app/app.js
@@ -2,12 +2,19 @@ angular.module('sunlook', [])
 
   .controller('firstController', function() {
     this.model = {
-      style: "straight",
-      color: "red",
-      skirt: "skirt type 1",
-      sleeve: "sleeve type 1",
-      decollete: "decollete type 1",
-      textile: "silk",
-      size: "XS"
+      style: 'straight',
+      color: 'red',
+      skirt: 'skirt type 1',
+      sleeve: 'sleeve type 1',
+      decollete: 'decollete type 1',
+      textile: 'silk',
+      size: 'XS'
     };
+    this.styleColor = {
+      straight: 'red',
+      slipOver: 'blue',
+      vStyle: 'pink'
+    };
+
+    var color = this.styleColor[this.model.style];
   })
