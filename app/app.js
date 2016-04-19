a7d82a63b6e417024842d4a573d1c7d7dfbcb971
Anastasiia Shvetsova <vitalii.kozyriev@trintix.com>
2016-04-19 14:49:47 +0300

    added images

--- app/app.js
+++ app/app.js
@@ -1,8 +1,34 @@
 angular.module('sunlook', [])
 
   .controller('firstController', function() {
-    this.model = {
-      style: 'straight',
+    var vm = this;
+    // this.styleColor = {
+    //   straight: 'red',
+    //   slipOver: 'blue',
+    //   vStyle: 'pink'
+    // };
+    vm.styles = [
+      {
+        type: 'straight',
+        bgColor: '#f00',
+        title: 'Straight',
+        image:  'app/images/style/straight.jpg'
+      },
+      {
+        type: 'slipOver',
+        bgColor: 'blue',
+        title: 'slipOver',
+        image: 'app/images/style/slip-over.jpg'
+      },
+      {
+        type: 'vStyle',
+        bgColor: 'pink',
+        title: 'slipOver',
+        image: 'app/images/style/slip-over.jpg'
+      }
+    ];
+    vm.model = {
+      style: vm.styles[0],
       color: 'red',
       skirt: 'skirt type 1',
       sleeve: 'sleeve type 1',
@@ -10,11 +36,5 @@ angular.module('sunlook', [])
       textile: 'silk',
       size: 'XS'
     };
-    this.styleColor = {
-      straight: 'red',
-      slipOver: 'blue',
-      vStyle: 'pink'
-    };
-
-    var color = this.styleColor[this.model.style];
+    // var color = this.styleColor[this.model.style];
   })
