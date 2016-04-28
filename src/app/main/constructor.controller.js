f544dabad99f4e13ded99f8972aca98d7b455d30
Anastasiia Shvetsova <vitalii.kozyriev@trintix.com>
2016-04-28 18:09:46 +0300

    added array decolette, skirt and sleeve

--- src/app/main/constructor.controller.js
+++ src/app/main/constructor.controller.js
@@ -4,77 +4,163 @@
 	angular.module('sunlook')
 		.controller('AppController', function() {
 		    var vm = this;
-		    // this.styleColor = {
-		    //   straight: 'red',
-		    //   slipOver: 'blue',
-		    //   vStyle: 'pink'
-		    // };
+
 		    vm.styles = [
 		      {
 		        type: 'straight',
 		        bgColor: '#f00',
 		        title: 'Straight',
-		        image:  '/src/images/style/straight.jpg'
+		        image:  '/src/images/style/straight.png'
 		      },
 		      {
 		        type: 'slipOver',
 		        bgColor: 'blue',
 		        title: 'slipOver',
-		        image: '/src/images/style/slip-over.jpg'
+		        image: '/src/images/style/slip-over.png'
 		      },
 		      {
 		        type: 'vStyle',
 		        bgColor: 'pink',
 		        title: 'slipOver',
-		        image: '/src/images/style/vStyle.jpg'
+		        image: '/src/images/style/vStyle.png'
 		      }
 		    ];
-		    vm.textiles = [
+
+			vm.textiles = [
+				{
+					title: 'silk',
+					image: '/src/images/textile/silk.jpg'
+				},
+				{
+					title: 'knitwear',
+					image: '/src/images/textile/knitwear.jpg'
+				},
+				{
+					title: 'coton',
+					image: '/src/images/textile/coton.jpg'
+				},
+				{
+					title: 'linen',
+					image: '/src/images/textile/linen.jpg'
+				},
+				{
+					title: 'chiffon',
+					image: '/src/images/textile/chiffon.jpg'
+				}
+			];
+
+			vm.decolletes = [
 		    	{
-		    		title: 'silk',
-		    		image: '/src/images/textile/silk.jpg'
+		    		title: 'scoop',
+		    		image: '/src/images/decollete/scoop.png'
 		    	},
 		    	{
-		    		title: 'knitwear',
-		    		image: '/src/images/textile/knitwear.jpg'
+		    		title: 'square',
+		    		image: '/src/images/decollete/square.png'
 		    	},
 		    	{
-		    		title: 'coton',
-		    		image: '/src/images/textile/coton.jpg'
+		    		title: 'v-neck',
+		    		image: '/src/images/decollete/v-neck.png'
 		    	},
 		    	{
-		    		title: 'linen',
-		    		image: '/src/images/textile/linen.jpg'
+		    		title: 'jewel',
+		    		image: '/src/images/decollete/jewel.png'
 		    	},
 		    	{
-		    		title: 'chiffon',
-		    		image: '/src/images/textile/chiffon.jpg'
+		    		title: 'jewel',
+		    		image: '/src/images/decollete/sabrina.png'
 		    	}
-		    ]
+		    ];
+
+		    vm.skirts = [
+		    	{
+		    		styleType: 'straight',
+		    		title: 'pencil',
+		    		image: '/src/images/skirts/pencil.png'
+		    	},
+		    	{
+		    		styleType: 'straight',
+		    		title: 'tube',
+		    		image: '/src/images/skirts/tube.png'
+		    	},
+		    	{
+		    		styleType: 'slipOver',
+		    		title: 'circle',
+		    		image: '/src/images/skirts/circle.png'
+		    	},
+		    	{
+		    		styleType: 'slipOver',
+		    		title: 'a-line',
+		    		image: '/src/images/skirts/a-line.png'
+		    	},
+		    	{
+		    		styleType: 'vStyle',
+		    		title: 'accordion',
+		    		image: '/src/images/skirts/accordion.png'
+		    	}
+		    ];
+
+		    vm.sleeves = [
+		    	{
+		    		title: 'bell',
+		    		image: '/src/images/sleeve/bell.png'
+		    	},
+		    	{
+		    		title: 'tube',
+		    		image: '/src/images/sleeve/bracelet.png'
+		    	},
+		    	{
+		    		title: 'circle',
+		    		image: '/src/images/sleeve/puffed.png'
+		    	},
+		    	{
+		    		title: 'a-line',
+		    		image: '/src/images/sleeve/roll-up.png'
+		    	},
+		    	{
+		    		title: 'accordion',
+		    		image: '/src/images/sleeve/shirt.png'
+		    	}
+		    ];
+
 		    vm.model = {
-		      style: vm.styles[0],
-		      color: 'red',
-		      skirt: 'skirt type 1',
-		      sleeve: 'sleeve type 1',
-		      decollete: 'decollete type 1',
-		      textile: 'silk',
-		      size: 'XS'
-		    };
+				style: vm.styles[0],
+				textile: vm.textiles[0],
+				decollete: vm.decolletes[0],
+				color: 'red',
+				skirt: vm.skirts[0],
+				sleeve: vm.sleeves[0],
+				size: 'XS'
+			};
+
 		   	vm.modalOpen = false;
 		    vm.showModal = function () {
 		     	vm.modalOpen = !vm.modalOpen;
 			}
+
 			vm.closeModal = function () {
 		     	vm.modalOpen = false;
 			}
+
 			vm.modelProperty = 'style';
 			vm.isActive = function (modelProperty) {
 				return vm.modelProperty === modelProperty;
 			}
+
 			vm.activated = function (modelProperty){
 				vm.modelProperty = modelProperty;
 			}
-	
+			
+			
+			vm.activateCheck = function (style) {
+				if ( vm.styles.type ==  'straight' ){
+					vm.checked = true;
+				} else {
+					vm.checked = false;
+				}
+			}
+			
+
 		 });
 
 })();
